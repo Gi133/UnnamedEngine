d8797a0275bd4e16cae2e563b70b11f80d453885
Andreas Xirotyris <xkostascy@hotmail.com>
2021-03-08 18:41:00 +0100

    Add project files.

---
+++ UnnamedEngine/UnnamedEngine.cpp
@@ -0,0 +1,12 @@
+﻿// UnnamedEngine.cpp : Defines the entry point for the application.
+//
+
+#include "UnnamedEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
