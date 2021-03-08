055ddbacebf263741de4e70a948a35d07e181cad
Andreas Xirotyris <xkostascy@hotmail.com>
2021-03-08 21:08:07 +0100

    Init Commit

---
+++ source/unnamedengine/UnnamedEngine.cpp
@@ -0,0 +1,29 @@
+﻿#include "UnnamedEngine.h"
+
+int main(int argc, char* argv[])
+{
+	SDL_Init(SDL_INIT_VIDEO);
+
+	auto* windowPtr = SDL_CreateWindow(
+		"Unnamed Engine"
+		, SDL_WINDOWPOS_UNDEFINED
+		, SDL_WINDOWPOS_UNDEFINED
+		, 800
+		, 600
+		, SDL_WINDOW_OPENGL
+	);
+
+	if (windowPtr == nullptr)
+	{
+		std::printf("Could not create window: %s", SDL_GetError());
+		return 1;
+	}
+
+	// Add application stuff here.
+
+	SDL_Delay(3000); // Yolo
+
+	SDL_DestroyWindow(windowPtr);
+
+	return 0;
+}
--- source/unnamedengine/UnnamedEngine.h
+++ source/unnamedengine/UnnamedEngine.h
@@ -4,5 +4,6 @@
 #pragma once
 
 #include <iostream>
+#include <SDL.h>
 
 // TODO: Reference additional headers your program requires here.
